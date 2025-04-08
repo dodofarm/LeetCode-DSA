a34de930f09fc9755ce01c84e1f1f66e6ff784de
dodofarm <ea01052002@gmail.com>
2025-04-08 21:04:32 +0100

    Modified: 215

--- 215.kth-largest-element-in-an-array.py
+++ 215.kth-largest-element-in-an-array.py
@@ -11,7 +11,7 @@ class Solution:
         arr = nums.copy()
         k = len(nums) - k
 
-        def quick_select(start: int, end: int) -> int:
+        def _quick_select(start: int, end: int) -> int:
             pointer = start
             pivot = arr[end]
             for i in range(start, end):
@@ -21,15 +21,14 @@ class Solution:
 
             arr[pointer], arr[end] = arr[end], arr[pointer]
 
-            if pointer > k:
-                return quick_select(start, pointer - 1)
+            if pointer > k:  # pylint: disable=R1705
+                return _quick_select(start, pointer - 1)
             elif pointer < k:
-                return quick_select(pointer + 1, end)
+                return _quick_select(pointer + 1, end)
             else:
                 return arr[pointer]
 
-        return quick_select(0, len(arr) - 1)
+        return _quick_select(0, len(arr) - 1)
 
 
 # @leet end
-
