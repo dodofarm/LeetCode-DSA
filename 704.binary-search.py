8e9e421c6e258a4a517544c2eb876bef795fb734
dodofarm <ea01052002@gmail.com>
2025-04-17 03:35:59 +0100

    Modified binary search

--- 704.binary-search.py
+++ 704.binary-search.py
@@ -6,7 +6,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
         while left <= right:
-            mid = left + (right - left) // 2
+            mid = (left + right) // 2
             if nums[mid] < target:
                 left = mid + 1
             elif nums[mid] > target:
@@ -18,4 +18,3 @@ class Solution:
 
 
 # @leet end
-
--- Algorithms/BinarySearch.py
+++ Algorithms/BinarySearch.py
@@ -5,7 +5,7 @@ def binary_search(arr: List[int], val: int) -> int:
     left, right = 0, len(arr) - 1
 
     while left <= right:
-        mid = left + (right - left) // 2
+        mid = (left + right) // 1
         if arr[mid] > val:
             right = mid - 1
         elif arr[mid] < val:
