3c314f0fc63de228b221a7acaa480f34046e62ce
dodofarm <ea01052002@gmail.com>
2025-05-05 02:46:36 +0100

    Added 125 and some tips on ASCII values

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -1,6 +1,5 @@
-# Nonoptimal solution - try again!
-# @leet start
-class Solution:
+# Nonoptimal solution
+class Solution_one:
     def isPalindrome(self, s: str) -> bool:
         start = 0
         end = len(s) - 1
@@ -20,6 +19,27 @@ class Solution:
 
             start += 1
             end -= 1
+
+        return True
+
+
+# @leet start
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        p1, p2 = 0, len(s) - 1
+
+        while p1 < p2:
+            if not s[p1].isalnum():
+                p1 += 1
+                continue
+            if not s[p2].isalnum():
+                p2 -= 1
+                continue
+
+            if s[p1].lower() != s[p2].lower():
+                return False
+            p1 += 1
+            p2 -= 1
 
         return True
 
