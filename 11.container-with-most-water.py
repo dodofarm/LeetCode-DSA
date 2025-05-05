0605818c62b30430d5a6a9578bf59ed8f9e4304f
dodofarm <ea01052002@gmail.com>
2025-05-05 02:22:29 +0100

    Added 11

---
+++ 11.container-with-most-water.py
@@ -0,0 +1,17 @@
+# @leet start
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        p1, p2 = 0, len(height) - 1
+        max_area = 0
+
+        while p1 < p2:
+            max_area = max(max_area, min(height[p1], height[p2]) * (p2 - p1))
+            if height[p1] < height[p2]:
+                p1 += 1
+            else:
+                p2 -= 1
+
+        return max_area
+
+
+# @leet end
